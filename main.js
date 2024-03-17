7d12e4bb6b78506b2b57d4175e33c15699950fde
Lazizbek <oddiybolakay05@gmail.com>
2024-03-18 00:18:10 +0500

    add all

---
+++ main.js
@@ -0,0 +1,25 @@
+let input = document.querySelector("#input");
+let button = document.querySelector("#btn")
+let text = document.querySelector("#text");
+let container = document.querySelector(".container");
+let body = document.querySelector("body");
+
+container.style.cssText = "max-width: 1440px;  text-align: center; margin: 50px auto;";
+body.style.background = "#212121";
+text.style.textAlign = "left";
+
+input.addEventListener("change", (event) => {
+    let inputValue = event.target.value;
+    
+    if (event.target.value != false) {
+        text.innerHTML += `<li>${inputValue}</li>`;
+    };
+
+    event.target.value = "";
+});
+
+button.addEventListener("click", (event) => {
+  if (event.target.value != "") {
+    text.innerHTML += `<li>${inputValue}</li>`;
+  }
+});
